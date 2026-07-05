59f2ecc9e529792e67117180775b1397015a817e
jb4r6z8 <97252913+jb4r6z8@users.noreply.github.com>
2026-07-05 09:19:00 +0200

    Update sensor.py

--- components/empty_sensor/sensor.py
+++ components/empty_sensor/sensor.py
@@ -1,17 +1,30 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
-from esphome.const import UNIT_EMPTY, ICON_EMPTY
+from esphome.const import UNIT_CELSIUS, ICON_THERMOMETER, DEVICE_CLASS_TEMPERATURE
 
-empty_sensor_ns = cg.esphome_ns.namespace("empty_sensor")
+# Create a namespace for our component
+my_sensor_ns = cg.esphome_ns.namespace("empty_sensor")
+MySensor = my_sensor_ns.class_("MySensor", sensor.Sensor, cg.PollingComponent)
 
-EmptySensor = empty_sensor_ns.class_("EmptySensor", sensor.Sensor, cg.PollingComponent)
+# Configuration schema
+CONFIG_SCHEMA = (
+    sensor.sensor_schema(
+        unit_of_measurement=UNIT_CELSIUS,
+        icon=ICON_THERMOMETER,
+        accuracy_decimals=1,
+        device_class=DEVICE_CLASS_TEMPERATURE
+    )
+    .extend(
+        {
+            cv.Optional("update_interval", default="60s"): cv.update_interval,
+        }
+    )
+    .extend(cv.polling_component_schema("60s"))
+)
 
-CONFIG_SCHEMA = sensor.sensor_schema(
-    EmptySensor, unit_of_measurement=UNIT_EMPTY, icon=ICON_EMPTY, accuracy_decimals=1
-).extend(cv.polling_component_schema("60s"))
-
-
+# Code generation
 async def to_code(config):
-    var = await sensor.new_sensor(config)
+    var = cg.new_Pvariable(config[cv.CONF_ID])
     await cg.register_component(var, config)
+    await sensor.register_sensor(var, config)
