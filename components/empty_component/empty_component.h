baa425dc0d9818f3054016f3ddc5c9b4dcf0001c
jb4r6z8 <97252913+jb4r6z8@users.noreply.github.com>
2026-07-05 16:23:53 +0200

    Update empty_component.h

--- components/empty_component/empty_component.h
+++ components/empty_component/empty_component.h
@@ -10,6 +10,11 @@ class EmptyComponent : public Component {
   void setup() override;
   void loop() override;
   void dump_config() override;
+
+  void adddata();
+ protected:
+  std::array<std::array<int32_t,90>,${gc_series}> data_;
+
 };
 
 
