6df50c9db6afdc010a257cea683f52463a4398f8
jb4r6z8 <97252913+jb4r6z8@users.noreply.github.com>
2026-07-05 19:30:39 +0200

    Update empty_component.cpp

--- components/empty_component/empty_component.cpp
+++ components/empty_component/empty_component.cpp
@@ -1,10 +1,5 @@
 #include "esphome/core/log.h"
 #include "empty_component.h"
-#include "esp_system.h"
-#include "esp_heap_caps.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "esp_spiram.h"
 
 namespace esphome {
 namespace empty_component {
@@ -24,13 +19,6 @@ void EmptyComponent::dump_config(){
 }
 
 void EmptyComponent::adddata(){
-    
-    // Check if PSRAM is available
-    if (!esp_spiram_is_initialized()) {
-        ESP_LOGE(TAG, "PSRAM not initialized!");
-        // Depending on the application, you might want to halt or handle this error
-        return;
-    }
     
   for (uint16_t i = 0; i < 10; i++ ) {
     std::vector<int32_t> v_vector_inside = {1,2,3,4};
