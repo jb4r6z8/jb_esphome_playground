e6a4e9b864b61478a38a161d46ef8af8c0387d11
jb4r6z8 <97252913+jb4r6z8@users.noreply.github.com>
2026-07-06 04:59:53 +0200

    VSCODE

--- components/helper_display/__init__.py
+++ components/helper_display/__init__.py
@@ -2,12 +2,12 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
 
-empty_component_ns = cg.esphome_ns.namespace("empty_component")
-EmptyComponent = empty_component_ns.class_("EmptyComponent", cg.Component)
+helper_display_ns = cg.esphome_ns.namespace("helper_display")
+HelperDisplay = helper_display_ns.class_("HelperDisplay", cg.Component)
 
 CONFIG_SCHEMA = cv.Schema(
     {
-        cv.GenerateID(): cv.declare_id(EmptyComponent),
+        cv.GenerateID(): cv.declare_id(HelperDisplay),
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
