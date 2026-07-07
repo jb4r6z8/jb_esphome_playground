4a81f101adb9d009111f397c65dbf300f0dd18f5
jb4r6z8 <97252913+jb4r6z8@users.noreply.github.com>
2026-07-07 11:21:06 +0200

    VSCODE

--- components/helper_display/hd_datasource.cpp
+++ components/helper_display/hd_datasource.cpp
@@ -4,7 +4,7 @@
 namespace esphome {
 namespace helper_display {
 
-void HD_DataSource::set_entity() {
+void HD_Datasource::set_entity() {
     ESP_LOGD("JBDS","entity set");
     entity_ = "Test";
 }
