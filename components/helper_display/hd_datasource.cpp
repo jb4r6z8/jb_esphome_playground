cdac4fe53e15a632adf086134afec41b43cce2d2
jb4r6z8 <97252913+jb4r6z8@users.noreply.github.com>
2026-07-07 20:50:02 +0200

    VSCODE

--- components/helper_display/hd_datasource.cpp
+++ components/helper_display/hd_datasource.cpp
@@ -24,8 +24,8 @@ HDDatasource::HDDatasource(std::string entity, uint16_t granularity) {
 
 void HDDatasource::update(int32_t value, bool force_append = false ) {
     if (force_append) {
-      ptr_ = ( ptr_ + 1 ) % data_size;
       ESP_LOGD("JBDS","1) PTR: %i value: %i", ptr_, data_[ptr_]);
+      ptr_ = ( ptr_ + 1 ) % data_size;
     } else {
       auto now = std::chrono::system_clock::now();
       auto now_ts = std::chrono::duration_cast<std::chrono::seconds>(now.time_since_epoch()).count();
