ed3b289d81bd555ecdde669b5fb647dc20ed6c5e
jb4r6z8 <97252913+jb4r6z8@users.noreply.github.com>
2026-07-07 17:21:26 +0200

    VSCODE

--- components/helper_display/hd_datasource.h
+++ components/helper_display/hd_datasource.h
@@ -11,7 +11,12 @@ class HDDatasource {
     void set_entity(std::string entity);
 
   protected:
+    bool initialized_;
     std::string entity_;
+    int32_t granularity_;
+    int32_t *arr_ = (int32_t *) heap_caps_malloc(1024 * 256 * sizeof(int32_t), MALLOC_CAP_SPIRAM);
+
+
 };
 
 }  // namespace helper_display
