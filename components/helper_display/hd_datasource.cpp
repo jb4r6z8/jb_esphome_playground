e3011a33b89a1154f9543834a718911bfa01e4f4
jb4r6z8 <97252913+jb4r6z8@users.noreply.github.com>
2026-07-07 21:20:45 +0200

    VSCODE

--- components/helper_display/hd_datasource.cpp
+++ components/helper_display/hd_datasource.cpp
@@ -14,23 +14,16 @@ HDDatasource::HDDatasource(std::string entity, uint16_t granularity) {
 }
 
 HDDatasource::HDDatasource() : HDDatasource("", 0) {
-  ESP_LOGD("JBDS","Constructor PTR: %i value: %i", ptr_, data_[ptr_]);
 }
 
 
 void HDDatasource::update(int32_t value, bool force_append = false ) {
-    ESP_LOGD("JBDS","Entity: %s Granularity: %i", entity_, granularity_);
-    if (force_append) {
-      ESP_LOGD("JBDS","1) PTR: %i value: %i", ptr_, data_[ptr_]);
+    auto now = std::chrono::system_clock::now();
+    auto now_ts = std::chrono::duration_cast<std::chrono::seconds>(now.time_since_epoch()).count();
+    if (force_append or ts_ + granularity_ < now_ts ) {
       ptr_ = ( ptr_ + 1 ) % data_size;
-    } else {
-      auto now = std::chrono::system_clock::now();
-      auto now_ts = std::chrono::duration_cast<std::chrono::seconds>(now.time_since_epoch()).count();
-      if ( ts_ + granularity_ < now_ts ) {
-        ts_ = now_ts - (now_ts % granularity_);
-        ptr_ = ( ptr_ + 1 ) % data_size;
-      }
     }
+    ts_ = now_ts - (now_ts % granularity_);
     data_[ptr_] = value;
     ESP_LOGD("JBDS","PTR: %i value: %i", ptr_, data_[ptr_]);
 }
