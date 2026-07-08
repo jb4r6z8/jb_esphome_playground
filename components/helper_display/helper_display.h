dfb2b87d66b08038358a3e0a00e2f2e6f5b2e691
jb4r6z8 <97252913+jb4r6z8@users.noreply.github.com>
2026-07-08 18:13:57 +0200

    VSCODE

--- components/helper_display/helper_display.h
+++ components/helper_display/helper_display.h
@@ -24,6 +24,7 @@ class HelperDisplay : public Component {
   void adddata();
  protected:
   std::map<std::string,std::map<uint16_t,HDDatasource*>> hdds_;
+  std::map<std::string,std::map<uint16_t,HDChartSeries*>> hdcs_;
   
 
 
