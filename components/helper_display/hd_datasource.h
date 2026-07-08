876dade69d51d938f7865fb7e566385b147f46fa
jb4r6z8 <97252913+jb4r6z8@users.noreply.github.com>
2026-07-08 06:40:20 +0200

    VSCODE

--- components/helper_display/hd_datasource.h
+++ components/helper_display/hd_datasource.h
@@ -16,7 +16,6 @@ class HDDatasource {
     std::string get_entity();
     uint16_t get_granularity();
     void update(int32_t value, bool force_append);
-    void init_by_json(JsonObjectConst json);
 
   protected:
     bool initialized_ = false;
