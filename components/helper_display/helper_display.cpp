87bee5d5ae996efb1098eb32d785cada3f2158f5
jb4r6z8 <97252913+jb4r6z8@users.noreply.github.com>
2026-07-10 14:54:35 +0200

    VSCODE

--- components/helper_display/helper_display.cpp
+++ components/helper_display/helper_display.cpp
@@ -198,6 +198,7 @@ void HelperDisplay::cs_update_data(std::string series) {
         ESP_LOGD("JB", "Chart Update");
         if (hdcs_[series]->get_data_size() > 0) {
           if (hdds_.contains(hdcs_[series]->get_entity()) and hdds_[hdcs_[series]->get_entity()].contains(hdcs_[series]->get_granularity())) {
+            ESP_LOGD("JB", "Chart Update, Datasource Found");
             for (uint32_t i = 0; i < hdcs_[series]->get_data_size(); i++) {
               int32_t value = hdds_[hdcs_[series]->get_entity()][hdcs_[series]->get_granularity()]->get_data_point_by_offset(i + 1 - hdcs_[series]->get_data_size());
               hdcs_[series]->update_data_point(i, value );
