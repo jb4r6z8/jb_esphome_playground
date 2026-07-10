d19d15f3625078dfa957148103fa0c32bd61b27f
jb4r6z8 <97252913+jb4r6z8@users.noreply.github.com>
2026-07-10 15:06:46 +0200

    VSCODE

--- components/helper_display/helper_display.cpp
+++ components/helper_display/helper_display.cpp
@@ -199,6 +199,9 @@ void HelperDisplay::cs_update_data(std::string series) {
         if (hdcs_[series]->get_data_size() > 0) {
           ESP_LOGD("JB", "Chart Update, CS Data Size valid");
           ESP_LOGD("JB", "Chart Update, CS Data Size valid: entity: %s", hdcs_[series]->get_entity().c_str());
+          if ( hdds_.contains(hdcs_[series]->get_entity()) ) {
+            ESP_LOGD("JB", "Chart Update, Entity exists");
+          }
           
           if (hdds_.contains(hdcs_[series]->get_entity()) and hdds_[hdcs_[series]->get_entity()].contains(hdcs_[series]->get_granularity())) {
             ESP_LOGD("JB", "Chart Update, Datasource Found");
