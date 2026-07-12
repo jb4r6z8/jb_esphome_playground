84d2045900e3028bf213d04f90425a3bdb08e77e
jb4r6z8 <97252913+jb4r6z8@users.noreply.github.com>
2026-07-12 11:31:31 +0200

    VSCODE

--- components/helper_display/helper_display.cpp
+++ components/helper_display/helper_display.cpp
@@ -61,10 +61,11 @@ void HDDatasource::init_by_json(JsonObjectConst json) {
       data_[r] = v_value_i;
     }
   }
-}
-if (entity_ == "sensor.pvfrnsac_power" and granularity_ == 120 ) {
-  for (int i == 0; i < data_size; i++ ) {
-    ESP_LOGD("JB","i: %i value: %i",i, data_[i]);
+  if (entity_ == "sensor.pvfrnsac_power" and granularity_ == 120 ) {
+    for (int i == 0; i < data_size; i++ ) {
+      ESP_LOGD("JB","i: %i value: %i",i, data_[i]);
+    }
+
   }
 
 }
